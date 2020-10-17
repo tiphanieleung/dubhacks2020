d0c28815e2a0eb1544af5e40f0c8289384d8f61c
tiphanieleung <leungti@uw.edu>
2020-10-17 15:36:43 -0700

    implement router between pages in App.js

--- dubhacks2020/src/App.js
+++ dubhacks2020/src/App.js
@@ -1,27 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Board from './pages/Board';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/contact" exact component={Contact} />
+          <Route path="/board" exact component={Board} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+
+{/* <div className="App">
+<header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <p>
+    Edit <code>src/App.js</code> and save to reload.
+  </p>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+</header>
+</div> */}
