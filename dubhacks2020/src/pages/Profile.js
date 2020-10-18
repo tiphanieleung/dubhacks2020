8f0158b5b63193e52bec89b0aa10ca9bc233ffa4
tiphanieleung <leungti@uw.edu>
2020-10-17 21:17:07 -0700

    plug card into storm page

--- dubhacks2020/src/pages/Profile.js
+++ dubhacks2020/src/pages/Profile.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Buttons from './Buttons';
-import ProblemCard from '../components/problemCards';
 
 function Profile() {
     return (
         <div>PROFILE
             <Buttons />
-            <ProblemCard />
         </div>
     )
 }
--- dubhacks2020/src/pages/Storm.js
+++ dubhacks2020/src/pages/Storm.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import ProblemCard from '../components/problemCards';
 
 function Storm() {
     return (
-        <div>STORM</div>
+        <div>STORM
+            <ProblemCard />
+        </div>
+
     )
 }
 
