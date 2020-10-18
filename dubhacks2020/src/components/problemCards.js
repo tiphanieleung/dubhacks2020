3c30db2a087a62776e7a897b5cd81640f5581357
tiphanieleung <leungti@uw.edu>
2020-10-17 21:13:46 -0700

    add card component

--- dubhacks2020/src/components/problemCards.js
+++ dubhacks2020/src/components/problemCards.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Card, Button, CardHeader, CardFooter, CardBody,
     CardTitle, CardText } from 'reactstrap';
+import '../style.css';
 
 function ProblemCard() {
     return (
         <div>
         <Card style={{ width: '15rem', padding: '15px', borderStyle: 'solid', borderRadius: '15px'}}>
-          <CardHeader>Header</CardHeader>
+          <CardHeader><input style={{borderStyle: 'none'}} type='text' name='title' value={'test'} /></CardHeader>
           <CardBody>
             <CardTitle>Special Title Treatment</CardTitle>
             <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
