c026a4aa61384cd2e590b1ca37586836277c6dbe
Oscar Zeng <oscarczeng@gmail.com>
2020-10-18 01:26:19 -0700

    sukk

--- dubhacks2020/src/pages/Home.js
+++ dubhacks2020/src/pages/Home.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Buttons from './Buttons';
 import '../style.css';
+import flow from '../images/flow-main.svg';
+import flowD from '../images/flow-mainBot.svg';
+import sdown from '../images/scroll-downBoi.svg';
+import Fade from 'react-reveal/Fade';
+
 
 function Home() {
     return (
-        <div className="banner-text">
-            <h1>Map Your Mind</h1>
-            <div>Let your thoughts now freely, track your</div>
-            <div>thought process, and visualize your flow!</div>
+        <div style={{justifyContent: 'center', display: "flex", flexDirection: 'column'}} className="banner-text">
+            <Fade bottom>
+                <img src={flow} alt='Flow'/>
+                <img style={{paddingTop: 70}}src={flowD} alt='FlowD'/>
+            </Fade>
+            
+            <img style={{justifyContent: 'center', paddingTop}} src={sdown} alt='Sdown'/>
+            <h1 style={{padding: 500}}>
+                henlo
+            </h1>
         </div>
     )
 }
